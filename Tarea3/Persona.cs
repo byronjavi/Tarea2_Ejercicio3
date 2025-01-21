e71e151fa977a111f1b6c0263fe5202aee074c47
programacion_Estructurada <byronjavi@gmial.com>
2025-01-21 02:27:34 -0500

    Agregar archivos de proyecto.

---
+++ Tarea3/Persona.cs
@@ -0,0 +1,142 @@
+﻿namespace Tarea3
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string DNI;
+
+        public Persona()
+        {
+        }
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo nombre   **/
+        /*************************************************/
+
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo Edad     **/
+        /*************************************************/
+
+        public int Edad
+        {
+            get { return this.edad; }
+            set { this.edad = value; }
+        }
+
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo DNI      **/
+        /*************************************************/
+        public string Dni
+        {
+            get { return this.DNI; }
+            set { this.DNI = value; }
+        }
+    }
+
+
+    //Clase llamada punto, creada para representar un punto en el plano
+
+    public class Punto
+    {
+        private float x;
+        private float y;
+
+        public Punto()
+        {
+        }
+
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo x        **/
+        /*************************************************/
+
+        public float X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo y        **/
+        /*************************************************/
+
+        public float Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+    }
+
+
+    //Clase llamada circulo, contiene un método que permite calcular el area del ciruclo
+
+    public class Circulo
+    {
+        float x;
+        float y;
+        float radio;
+
+        public Circulo()
+        {
+        }
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo x        **/
+        /*************************************************/
+        public float X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo y        **/
+        /*************************************************/
+        public float Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+
+        /*************************************************/
+        /**  Metodo SET y GET para el atributo radio    **/
+        /*************************************************/
+        public float Radio
+        {
+            get { return this.radio; }
+            set { this.radio = value; }
+        }
+
+
+        /*************************************************/
+        /**  Metodo calcular el area del circulo        **/
+        /*************************************************/
+
+
+        public double areaCirculo(float rd)
+        {
+            //Area del circulo = PI * radio al cuadrado
+            const double pi = 3.1416;
+            double resultado = 0;
+
+            Circulo nue = new Circulo();
+
+            resultado = pi * (rd * rd);
+            return resultado;
+        }
+
+    }
+
+}
---
+++ Tarea3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se creo la clase persona con toda la informacion solicitada
+            //Se procede a utilizar el metodo que calcula el area del circulo
+            string continuar = "";
+
+            while (true)
+            {
+                Console.WriteLine("Este programa le permite calcular el area del circulo");
+
+                Circulo circulo = new Circulo();
+
+                Console.Write("Ingrese el radio del circulo: ");
+                circulo.Radio = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("El area del circulo es: " + circulo.areaCirculo(circulo.Radio));
+
+                Console.WriteLine("Desea continuar: s/n");
+                continuar = Console.ReadLine().ToLower();
+
+                if (continuar.Equals("n")) break;//
+            }
+        }
+
+    }
+}
